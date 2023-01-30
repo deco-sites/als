72ccd90e80a3d0c8a66f5d95ab32a8c8bd896777
igorbrasileiro <brasileiro456@gmail.com>
2023-01-30 13:59:24 -0300

    Drop cwv, error handler scripts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,23 @@
-import { live } from "$live/live.ts";
-// import LiveSections from "$live/components/LiveSections.tsx";
-// import { PageProps } from "$fresh/server.ts";
-// import { LivePageData } from "$live/types.ts";
-import LivePage from "$live/components/LivePage.tsx";
+import { context, live } from "$live/live.ts";
+import LiveSections from "$live/components/LiveSections.tsx";
+import { PageProps } from "$fresh/server.ts";
+import { LivePageData } from "$live/types.ts";
+import Jitsu from "partytown/integrations/Jitsu.tsx";
+// import LivePage from "$live/components/LivePage.tsx";
 
 export const handler = live();
 
-export default LivePage;
+export default function LivePage({ data }: PageProps<LivePageData>) {
+  return (
+    <>
+      {(context.isDeploy) &&
+        (
+          <Jitsu
+            data-init-only="true"
+            data-key="js.9wshjdbktbdeqmh282l0th.c354uin379su270joldy2"
+          />
+        )}
+      {!!data && <LiveSections {...data.page.data} />}
+    </>
+  );
+}
