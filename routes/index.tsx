0cd2f2685c54e0b302ef0ae8ace2884870242824
igorbrasileiro <brasileiro456@gmail.com>
2023-01-30 13:47:13 -0300

    Bump partytown scripts and add back

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,9 @@
 import { live } from "$live/live.ts";
-import LiveSections from "$live/components/LiveSections.tsx";
-import { PageProps } from "$fresh/server.ts";
-import { LivePageData } from "$live/types.ts";
+// import LiveSections from "$live/components/LiveSections.tsx";
+// import { PageProps } from "$fresh/server.ts";
+// import { LivePageData } from "$live/types.ts";
+import LivePage from "$live/components/LivePage.tsx";
 
 export const handler = live();
 
-export default function LivePage(
-  { data }: PageProps<LivePageData | undefined>,
-) {
-  return (
-    <>
-      {!!data && <LiveSections {...data.page.data} />}
-    </>
-  );
-}
+export default LivePage;
